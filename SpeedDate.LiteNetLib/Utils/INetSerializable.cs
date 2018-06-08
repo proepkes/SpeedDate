e82181f311ecc939c92c36b227f168b4e71e243b
proepkes <pb156875@fh-muenster.de>
2018-06-08 20:44:13 +0200

    Remove LiteNetLib.csproj

--- SpeedDate.LiteNetLib/Utils/INetSerializable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpeedDate.LiteNetLib.Utils
-{
-    public interface INetSerializable
-    {
-        void Serialize(NetDataWriter writer);
-        void Deserialize(NetDataReader reader);
-    }
-}
