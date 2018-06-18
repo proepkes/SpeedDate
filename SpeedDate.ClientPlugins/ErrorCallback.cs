77cd5537789f787694ee20022f8a5dcdc3b4658b
proepkes <pb156875@fh-muenster.de>
2018-06-19 00:00:48 +0200

    Added delegate ErrorCallback

---
+++ SpeedDate.ClientPlugins/ErrorCallback.cs
@@ -0,0 +1,8 @@
+﻿namespace SpeedDate.ClientPlugins
+{
+    /// <summary>
+    /// Generic error callback declaration.
+    /// </summary>
+    /// <param name="reason"></param>
+    public delegate void ErrorCallback(string reason);
+}
