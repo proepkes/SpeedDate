1af3c7dae579854c04e5b213b736cc378296ed8e
proepkes <pb156875@fh-muenster.de>
2018-06-22 20:46:20 +0200

    Uncommented

--- SpeedDate.Test/TestLobby.cs
+++ SpeedDate.Test/TestLobby.cs
@@ -15,6 +15,7 @@ using SpeedDate.ServerPlugins.Authentication;
 using SpeedDate.ServerPlugins.Lobbies;
 using Xunit;
 using Xunit.Abstractions;
+using LobbyState = SpeedDate.Packets.Lobbies.LobbyState;
 
 namespace SpeedDate.Test
 {
@@ -40,29 +41,29 @@ namespace SpeedDate.Test
             var lobbyCreator = new SpeedDateClient();
             lobbyCreator.Started += () =>
             {
-//                lobbyCreator.GetPlugin<AuthPlugin>().LogInAsGuest(info =>
-//                {
-//                    _output.WriteLine($"Logged in as {info.Username}");
-//                    lobbyCreator.GetPlugin<LobbyPlugin>().CreateAndJoin("3 vs 3", new Dictionary<string, string> {
-//                    {
-//                        OptionKeys.LobbyName, LOBBY_NAME
-//                    }}, lobby =>
-//                    {
-//                        lobby.LobbyName.ShouldBe(LOBBY_NAME);
-//                        lobby.State.ShouldBe(LobbyState.Preparations);
-//                        lobby.Id.ShouldBe(EXPECTED_LOBBY_ID);
-//
-//                        _output.WriteLine($"Lobby successfully created");
+                lobbyCreator.GetPlugin<AuthPlugin>().LogInAsGuest(info =>
+                {
+                    _output.WriteLine($"Logged in as {info.Username}");
+                    lobbyCreator.GetPlugin<LobbyPlugin>().CreateAndJoin("3 vs 3", new Dictionary<string, string> {
+                    {
+                        OptionKeys.LobbyName, LOBBY_NAME
+                    }}, lobby =>
+                    {
+                        lobby.LobbyName.ShouldBe(LOBBY_NAME);
+                        lobby.State.ShouldBe(LobbyState.Preparations);
+                        lobby.Id.ShouldBe(EXPECTED_LOBBY_ID);
+
+                        _output.WriteLine($"Lobby successfully created");
                         are.Set();
-//                    }, error =>
-//                    {
-//                        Should.NotThrow(() => throw new Exception(error));
-//                    });
-//                },
-//                error =>
-//                {
-//                    Should.NotThrow(() => throw new Exception(error));
-//                });
+                    }, error =>
+                    {
+                        Should.NotThrow(() => throw new Exception(error));
+                    });
+                },
+                error =>
+                {
+                    Should.NotThrow(() => throw new Exception(error));
+                });
             };
 
             server.Started += () =>
@@ -83,55 +84,55 @@ namespace SpeedDate.Test
                     }
                 })
             );
-//
-//            server.GetPlugin<LobbiesPlugin>().AddFactory(new LobbyFactoryAnonymous("3 vs 3", server.GetPlugin<LobbiesPlugin>(), DemoLobbyFactories.TwoVsTwoVsFour));
-//
-//            are.WaitOne(TimeSpan.FromSeconds(10)).ShouldBeTrue(); //Should be signaled, wait for lobbby-created
-//
-//            _output.WriteLine($"Starting LobbyJoiner...");
-//
-//            var lobbyJoiner = new SpeedDateClient();
-//            lobbyJoiner.Started += () =>
-//            {
-//                lobbyJoiner.GetPlugin<AuthPlugin>().LogInAsGuest(info =>
-//                    {
-//                        _output.WriteLine($"Logged in as {info.Username}");
-//                        lobbyJoiner.GetPlugin<MatchmakerPlugin>().FindGames(new Dictionary<string, string>(), games =>
-//                        {
-//                            games.Count.ShouldBe(1);
-//
-//                            var lobby = games.First();
-//                            lobby.Type.ShouldBe(GameInfoType.Lobby);
-//                            lobby.Id.ShouldBe(EXPECTED_LOBBY_ID);
-//                            lobby.Name.ShouldBe(LOBBY_NAME);
-//                            lobby.OnlinePlayers.ShouldBe(1);
-//
-//                            lobbyJoiner.GetPlugin<LobbyPlugin>().JoinLobby(lobby.Id, joinedLobby =>
-//                            {
-//                                joinedLobby.Id.ShouldBe(EXPECTED_LOBBY_ID);
-//                                joinedLobby.Members.Count.ShouldBe(2);
-//
-//                                _output.WriteLine($"Lobby successfully joined");
-//                                are.Set();
-//                            }, error =>
-//                            {
-//                                Should.NotThrow(() => throw new Exception(error));
-//                            });
-//                        }, error =>
-//                        {
-//                            Should.NotThrow(() => throw new Exception(error));
-//                        });
-//                    },
-//                    error =>
-//                    {
-//                        Should.NotThrow(() => throw new Exception(error));
-//                    });
-//            };
-//
-//            lobbyJoiner.Start(new DefaultConfigProvider(
-//                new NetworkConfig(IPAddress.Loopback, port), //Connect to port
-//                new PluginsConfig("SpeedDate.ClientPlugins.Peer*"))); //Load peer-plugins only
-//
+
+            server.GetPlugin<LobbiesPlugin>().AddFactory(new LobbyFactoryAnonymous("3 vs 3", server.GetPlugin<LobbiesPlugin>(), DemoLobbyFactories.TwoVsTwoVsFour));
+
+            are.WaitOne(TimeSpan.FromSeconds(10)).ShouldBeTrue(); //Should be signaled, wait for lobbby-created
+
+            _output.WriteLine($"Starting LobbyJoiner...");
+
+            var lobbyJoiner = new SpeedDateClient();
+            lobbyJoiner.Started += () =>
+            {
+                lobbyJoiner.GetPlugin<AuthPlugin>().LogInAsGuest(info =>
+                    {
+                        _output.WriteLine($"Logged in as {info.Username}");
+                        lobbyJoiner.GetPlugin<MatchmakerPlugin>().FindGames(new Dictionary<string, string>(), games =>
+                        {
+                            games.Count.ShouldBe(1);
+
+                            var lobby = games.First();
+                            lobby.Type.ShouldBe(GameInfoType.Lobby);
+                            lobby.Id.ShouldBe(EXPECTED_LOBBY_ID);
+                            lobby.Name.ShouldBe(LOBBY_NAME);
+                            lobby.OnlinePlayers.ShouldBe(1);
+
+                            lobbyJoiner.GetPlugin<LobbyPlugin>().JoinLobby(lobby.Id, joinedLobby =>
+                            {
+                                joinedLobby.Id.ShouldBe(EXPECTED_LOBBY_ID);
+                                joinedLobby.Members.Count.ShouldBe(2);
+
+                                _output.WriteLine($"Lobby successfully joined");
+                                are.Set();
+                            }, error =>
+                            {
+                                Should.NotThrow(() => throw new Exception(error));
+                            });
+                        }, error =>
+                        {
+                            Should.NotThrow(() => throw new Exception(error));
+                        });
+                    },
+                    error =>
+                    {
+                        Should.NotThrow(() => throw new Exception(error));
+                    });
+            };
+
+            lobbyJoiner.Start(new DefaultConfigProvider(
+                new NetworkConfig(IPAddress.Loopback, port), //Connect to port
+                new PluginsConfig("SpeedDate.ClientPlugins.Peer*"))); //Load peer-plugins only
+
             are.WaitOne(TimeSpan.FromSeconds(10)).ShouldBeTrue(); //Should be signaled, wait for lobbby-joined
 
             server.Stop();
