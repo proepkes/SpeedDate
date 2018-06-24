cbb3761c5f21ed8a893ee318e2f8c2dc6f1a044c
proepkes <pb156875@fh-muenster.de>
2018-06-24 16:59:30 +0200

    Nunit

---
+++ SpeedDate.Test/Setup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using SpeedDate.Configuration;
+using SpeedDate.Server;
+using SpeedDate.ServerPlugins.Authentication;
+
+namespace SpeedDate.Test
+{
+    public class ServerFixture: IDisposable
+    {
+        public const string GuestPrefix = "TestGuest-";
+        
+        public readonly int Port = 12345;
+        public readonly SpeedDateServer Server;
+        
+        public ServerFixture()
+        {
+            Server = new SpeedDateServer();
+            
+            Server.Start(new DefaultConfigProvider(new NetworkConfig(IPAddress.Any, Port), PluginsConfig.DefaultServerPlugins, new []
+            {
+                new AuthConfig
+                {
+                    GuestPrefix = GuestPrefix,
+                    EnableGuestLogin = true
+                }
+            }));
+        }
+        
+        public void Dispose()
+        {
+            Server.Stop();
+            Server.Dispose();
+        }
+    }
+}
