1aad0443911e43f94efed355bd70d02eebca0906
proepkes <pb156875@fh-muenster.de>
2018-07-04 22:45:11 +0200

    Default values for AuthConfig

--- SpeedDate.ServerPlugins/Authentication/AuthConfig.cs
+++ SpeedDate.ServerPlugins/Authentication/AuthConfig.cs
@@ -4,11 +4,11 @@ namespace SpeedDate.ServerPlugins.Authentication
 {
     public class AuthConfig : IConfig
     {
-        public bool EnableGuestLogin { get; set; }
-        public string GuestPrefix { get; set; }
+        public bool EnableGuestLogin { get; set; } = true;
+        public string GuestPrefix { get; set; } = "Guest-";
         public int PeerDataPermissionsLevel { get; set; }
-        public int UsernameMinChars { get; set; }
-        public int UsernameMaxChars { get; set; }
+        public int UsernameMinChars { get; set; } = 3;
+        public int UsernameMaxChars { get; set; } = 12;
         public string ConfirmEmailBody { get; set; }
         public string PasswordResetEmailBody { get; set; }
     }
