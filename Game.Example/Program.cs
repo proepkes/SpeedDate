3eead1d12ff62b839d53c7cf54fd638d583d0520
proepkes <pb156875@fh-muenster.de>
2018-07-15 00:53:58 +0200

    Added example-game as dotnet-core project

---
+++ Game.Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ConsoleGame;
+using SpeedDate;
+
+namespace Game.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine($"Starting game with arguments: {string.Join(", ", args)}...");
+
+            var server = new GameServer();
+            server.ConnectedToMaster += () =>
+            {
+                Console.WriteLine("Connected to Master");
+                server.Rooms.RegisterSpawnedProcess(
+                    CommandLineArgs.SpawnId,
+                    CommandLineArgs.SpawnCode,
+                    (controller) =>
+                    {
+                        Console.WriteLine("Registered to Master");
+                    }, Console.WriteLine);
+            };
+
+            server.Start("GameServerConfig.xml");
+
+            Console.ReadLine();
+        }
+    }
+}
