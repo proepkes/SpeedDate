6b97c374bacf045b713195878cd7e29a85c2f123
proepkes <phil.boerner@fh-muenster.de>
2018-11-13 00:17:34 +0100

    auther.auth

--- authsvc/auth.go
+++ authsvc/auth.go
@@ -2,7 +2,6 @@ package authsvc
 
 import (
 	"context"
-	"fmt"
 
 	"goa.design/goa/security"
 )
@@ -10,19 +9,6 @@ import (
 // BasicAuth implements the authorization logic for service "authorize" for the
 // "basic" security scheme.
 func (s *authorizeSvc) BasicAuth(ctx context.Context, user, pass string, scheme *security.BasicScheme) (context.Context, error) {
-	//
-	// TBD: add authorization logic.
-	//
-	// In case of authorization failure this function should return
-	// one of the generated error structs, e.g.:
-	//
-	//    return ctx, myservice.MakeUnauthorizedError("invalid token")
-	//
-	// Alternatively this function may return an instance of
-	// goa.ServiceError with a Name field value that matches one of
-	// the design error names, e.g:
-	//
-	//    return ctx, goa.PermanentError("unauthorized", "invalid token")
-	//
-	return ctx, fmt.Errorf("not implemented")
+	//TODO: check user and pass against database
+	return ctx, nil
 }
