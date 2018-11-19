9eb027ef99b5da896c3ee6f011d759e38dcf8818
proepkes <phil.boerner@fh-muenster.de>
2018-11-19 19:57:23 +0100

    gen

---
+++ src/pkg/apis/dev/v1/gameserver.go
@@ -0,0 +1,21 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type GameServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type GameServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []GameServer `json:"items"`
+}
