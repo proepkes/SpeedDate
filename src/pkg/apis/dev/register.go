eac73d0825fed5973124220c13fb348ca459c1c4
proepkes <phil.boerner@fh-muenster.de>
2018-11-19 19:57:06 +0100

    gen

---
+++ src/pkg/apis/dev/register.go
@@ -0,0 +1,5 @@
+package dev
+
+const (
+	GroupName = "dev.speeddate"
+)
---
+++ src/pkg/apis/dev/v1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package,register
+
+// Package v1 is the v1 version of the API.
+// +groupName=dev.speeddate
+
+package v1
