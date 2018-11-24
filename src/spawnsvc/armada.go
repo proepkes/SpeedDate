aa9cfc769d1129a6245b4d5085a492aecba18cd2
proepkes <phil.boerner@gmx.de>
2018-11-24 21:11:15 +0100

    ..

--- src/spawnsvc/armada.go
+++ src/spawnsvc/armada.go
@@ -2,7 +2,6 @@ package spawnsvc
 
 import (
 	"context"
-	"log"
 
 	armada "github.com/proepkes/speeddate/src/spawnsvc/gen/armada"
 )
@@ -10,16 +9,15 @@ import (
 // armada service example implementation.
 // The example methods log the requests and return zero values.
 type armadaSvc struct {
-	logger *log.Logger
 }
 
 // NewArmada returns the armada service implementation.
-func NewArmada(logger *log.Logger) armada.Service {
-	return &armadaSvc{logger}
+func NewArmada() armada.Service {
+	return &armadaSvc{}
 }
 
 // Add a new gameserver to the armada.
 func (s *armadaSvc) Add(ctx context.Context) (res string, err error) {
-	s.logger.Print("armada.add")
+
 	return
 }
--- src/spawnsvc/cmd/spawner/main.go
+++ src/spawnsvc/cmd/spawner/main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"net/http"
 	"os"
-	"os/signal"
 	"time"
 
 	homedir "github.com/mitchellh/go-homedir"
-	"github.com/proepkes/speeddate/src/spawnsvc"
-	armada "github.com/proepkes/speeddate/src/spawnsvc/gen/armada"
-	armadasvr "github.com/proepkes/speeddate/src/spawnsvc/gen/http/armada/server"
-	swaggersvr "github.com/proepkes/speeddate/src/spawnsvc/gen/http/swagger/server"
 	clientset "github.com/proepkes/speeddate/src/spawnsvc/pkg/client/clientset/versioned"
 	"github.com/proepkes/speeddate/src/spawnsvc/pkg/gs"
 	"github.com/proepkes/speeddate/src/spawnsvc/pkg/signals"
-	goahttp "goa.design/goa/http"
-	"goa.design/goa/http/middleware"
 
 	glog "github.com/golang/glog"
 	informers "github.com/proepkes/speeddate/src/spawnsvc/pkg/client/informers/externalversions"
@@ -46,21 +37,21 @@ func getClientLocal() (kubernetes.Interface, *clientset.Clientset) {
 	// create the config from the path
 	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
-		log.Fatalf("getClusterConfig: %v", err)
+		glog.Fatalf("getClusterConfig: %v", err)
 	}
 
 	// generate the client based off of the config
 	k8sClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("getClusterConfig: %v", err)
+		glog.Fatalf("getClusterConfig: %v", err)
 	}
 
 	client, err := clientset.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Error building example clientset: %s", err.Error())
+		glog.Fatalf("Error building example clientset: %s", err.Error())
 	}
 
-	log.Println("Successfully constructed k8s client")
+	glog.Infoln("Successfully constructed k8s client")
 
 	return k8sClient, client
 }
@@ -68,32 +59,26 @@ func getClientLocal() (kubernetes.Interface, *clientset.Clientset) {
 func getClientInCluster() (kubernetes.Interface, *clientset.Clientset) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalf("getClusterConfig: %v", err)
+		glog.Fatalf("getClusterConfig: %v", err)
 	}
 
 	// generate the client based off of the config
 	k8sClient, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("getClusterConfig: %v", err)
+		glog.Fatalf("getClusterConfig: %v", err)
 	}
 
 	client, err := clientset.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Error building example clientset: %s", err.Error())
+		glog.Fatalf("Error building example clientset: %s", err.Error())
 	}
 
-	log.Println("Successfully constructed k8s client")
+	glog.Infoln("Successfully constructed k8s client")
 
 	return k8sClient, client
 }
 
 func main() {
-	// Define command line flags, add any other flag required to configure
-	// the service.
-	var (
-		addr = flag.String("listen", ":8001", "HTTP listen `address`")
-		dbg  = flag.Bool("debug", false, "Log request and response bodies")
-	)
 	flag.Parse()
 
 	// Create go-kit logger in your main.go
@@ -106,17 +91,6 @@ func main() {
 	// Thus we need to pass it our go-kit logger object.
 	glog.SetLogger(logger)
 
-	// Setup logger and goa log adapter. Replace logger with your own using
-	// your log package of choice.
-	var (
-		adapter middleware.Logger
-		// logger  *log.Logger
-	)
-	{
-		// logger = log.New(os.Stderr, "[spawnsvc] ", log.Ltime)
-		// adapter = middleware.NewLogger(logger)
-	}
-
 	// get the Kubernetes client for connectivity
 	k8sClient, client := getClientInCluster()
 
@@ -125,14 +99,6 @@ func main() {
 
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(k8sClient, time.Second*30)
 	exampleInformerFactory := informers.NewSharedInformerFactory(client, time.Second*30)
-
-	// Create the structs that implement the services.
-	var (
-		armadaSvc armada.Service
-	)
-	{
-		armadaSvc = spawnsvc.NewArmada(logger)
-	}
 
 	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
 	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
@@ -146,108 +112,6 @@ func main() {
 		exampleInformerFactory.Dev().V1().GameServers())
 
 	if err := gsController.Run(1, stopCh); err != nil {
-		klog.Info("Error running controller: %s", err.Error())
-	}
-	// Wrap the services in endpoints that can be invoked from other
-	// services potentially running in different processes.
-	var (
-		armadaEndpoints *armada.Endpoints
-	)
-	{
-		armadaEndpoints = armada.NewEndpoints(armadaSvc)
-	}
-
-	// Provide the transport specific request decoder and response encoder.
-	// The goa http package has built-in support for JSON, XML and gob.
-	// Other encodings can be used by providing the corresponding functions,
-	// see goa.design/encoding.
-	var (
-		dec = goahttp.RequestDecoder
-		enc = goahttp.ResponseEncoder
-	)
-	// Build the service HTTP request multiplexer and configure it to serve
-	// HTTP requests to the service endpoints.
-	var mux goahttp.Muxer
-	{
-		mux = goahttp.NewMuxer()
-	}
-	// Wrap the endpoints with the transport specific layers. The generated
-	// server packages contains code generated from the design which maps
-	// the service input and output data structures to HTTP requests and
-	// responses.
-	var (
-		armadaServer  *armadasvr.Server
-		swaggerServer *swaggersvr.Server
-	)
-	{
-		eh := ErrorHandler(logger)
-		armadaServer = armadasvr.New(armadaEndpoints, mux, dec, enc, eh)
-		swaggerServer = swaggersvr.New(nil, mux, dec, enc, eh)
-	}
-	// Configure the mux.
-	armadasvr.Mount(mux, armadaServer)
-	swaggersvr.Mount(mux)
-
-	// Wrap the multiplexer with additional middlewares. Middlewares mounted
-	// here apply to all the service endpoints.
-	var handler http.Handler = mux
-	{
-		if *dbg {
-			handler = middleware.Debug(mux, os.Stdout)(handler)
-		}
-		handler = middleware.Log(adapter)(handler)
-		handler = middleware.RequestID()(handler)
-	}
-
-	// Create channel used by both the signal handler and server goroutines
-	// to notify the main goroutine when to stop the server.
-	errc := make(chan error)
-	// Setup interrupt handler. This optional step configures the process so
-	// that SIGINT and SIGTERM signals cause the service to stop gracefully.
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		errc <- fmt.Errorf("%s", <-c)
-	}()
-
-	srvHealth := http.NewServeMux()
-	srvHealth.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ok"))
-	})
-	go func() {
-		http.ListenAndServe(":9000", srvHealth)
-	}()
-
-	// Start HTTP server using default configuration, change the code to
-	// configure the server as required by your service.
-	srv := &http.Server{Addr: *addr, Handler: handler}
-	go func() {
-		for _, m := range armadaServer.Mounts {
-			logger.Printf("method %q mounted on %s %s", m.Method, m.Verb, m.Pattern)
-		}
-		for _, m := range swaggerServer.Mounts {
-			logger.Printf("file %q mounted on %s %s", m.Method, m.Verb, m.Pattern)
-		}
-		logger.Printf("listening on %s", *addr)
-		errc <- srv.ListenAndServe()
-	}()
-
-	// Wait for signal.
-	logger.Printf("exiting (%v)", <-errc)
-	// Shutdown gracefully with a 30s timeout.
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-	srv.Shutdown(ctx)
-	logger.Println("exited")
-}
-
-// ErrorHandler returns a function that writes and logs the given error.
-// The function also writes and logs the error unique ID so that it's possible
-// to correlate.
-func ErrorHandler(logger *log.Logger) func(context.Context, http.ResponseWriter, error) {
-	return func(ctx context.Context, w http.ResponseWriter, err error) {
-		id := ctx.Value(middleware.RequestIDKey).(string)
-		w.Write([]byte("[" + id + "] encoding: " + err.Error()))
-		logger.Printf("[%s] ERROR: %s", id, err.Error())
+		klog.Infof("Error running controller: %s", err.Error())
 	}
 }
